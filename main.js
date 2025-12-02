5bded29eedf5935767cc27dd01cdb57ec4792b4f
ben-garcia-codes <garciabe@cua.edu>
2025-12-02 15:15:43 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-console.log("main.js is loaded!");
-alert("Welcome to Santa Teresa!");
-const menuItemsHTMLcollection = document.getElementsByClassName("Header");
-console.log(menuItemsHTMLcollection)
-
